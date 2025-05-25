a22f65f4f49f57da0fe1a535213182a19254f995
hernanpsc <136007081+hernanpsc@users.noreply.github.com>
2025-05-25 09:45:12 -0300

    Remove unused invoice generator

--- invoice-printer/invoice-generator.mjs
+++ invoice-printer/invoice-generator.mjs
@@ -1,11 +1,1 @@
-import fs from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-export default async function generateHtmlInvoice() {
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = path.dirname(__filename)
-
-    const htmlInvoice = await fs.readFile(__dirname + '/invoice.html', 'utf8');
-    return htmlInvoice
-}
+// This file can be safely deleted as it's no longer needed
